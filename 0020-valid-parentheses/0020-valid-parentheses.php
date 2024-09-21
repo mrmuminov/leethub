3c2e4d1da36729b49d468071174c082b997abc29
Bahriddin Mo'minov <64766486+mrmuminov@users.noreply.github.com>
2024-09-20 22:24:47 -0700

    Time: 41 ms (5.09%), Space: 19.7 MB (96.00%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.php
@@ -0,0 +1,17 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isValid($s) {
+        $i = true;
+        while($i) {
+            if (strpos($s, '[]') === false && strpos($s, '()') === false && strpos($s, '{}') === false) {
+                $i = false;
+            }
+            $s = str_replace(['[]', '{}', '()'], '', $s);
+        }
+        return strlen($s) === 0;
+    }
+}
