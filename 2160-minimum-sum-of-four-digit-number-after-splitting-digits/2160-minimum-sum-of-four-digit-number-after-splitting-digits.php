889c3bddbe153d03a7b9b2d5de1de2b0c884d0f6
Bahriddin Mo'minov <64766486+mrmuminov@users.noreply.github.com>
2024-09-20 22:03:39 -0700

    Time: 15 ms (5.88%), Space: 19.8 MB (82.35%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.php
@@ -0,0 +1,16 @@
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return Integer
+     */
+    function minimumSum($num) {
+        $num = str_split(str_replace('0', '', $num));
+        $c = count($num);
+        if ($c === 1) return $num[0];
+        if ($c === 2) return $num[0] + $num[1];
+        sort($num);
+        if ($c === 4) return ($num[0] . $num[3]) + ($num[1] . $num[2]);
+        return ($num[0] . $num[1]) + ($num[2]);
+    }
+}
