6b72954360f9c13ea1e143ec3426c38ab345b1b0
Bahriddin Mo'minov <64766486+mrmuminov@users.noreply.github.com>
2024-09-26 01:26:56 -0700

    Time: 18 ms (90.47%), Space: 20.7 MB (94.89%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.php
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.php
@@ -5,16 +5,17 @@ class Solution {
      * @return Integer
      */
     function removeDuplicates(&$nums) {
-        $l = count($nums);
+        $l = $s = count($nums);
         $n = false;
         for ($i=0;$i<$l;$i++) {
             if ($n !== $nums[$i]) {
                 $n = $nums[$i];
             } else {
+                $s--;
                 unset($nums[$i]);
             }
         }
         
-        return count($nums);
+        return $s;
     }
 }
