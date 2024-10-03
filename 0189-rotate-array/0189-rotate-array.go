4e02e10ea9e0c8f7c07e48726306d3fcf75a6b6c
Bahriddin Mo'minov <64766486+mrmuminov@users.noreply.github.com>
2024-10-03 00:57:23 -0700

    Time: 38 ms (13.34%), Space: 7.9 MB (45.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.go
+++ 0189-rotate-array/0189-rotate-array.go
@@ -1,16 +1,18 @@
 func rotate(nums []int, k int) {
 	n := len(nums)
 	k = k % n
-	reverse(nums, 0, n-1)
-	reverse(nums, 0, k-1)
-	reverse(nums, k, n-1)
-	fmt.Println(nums)
-}
-
-func reverse(nums []int, start, end int) {
-	for start < end {
-		nums[start], nums[end] = nums[end], nums[start]
-		start++
-		end--
+	count := 0
+	for start := 0; count < n; start++ {
+		current, prev := start, nums[start]
+		for {
+			next := (current + k) % n
+			nums[next], prev = prev, nums[next]
+			current = next
+			count++
+			if start == current {
+				break
+			}
+		}
 	}
+	fmt.Println(nums)
 }
